a157efeac4453cbd32f81cf166b702216921beef
kazmik05102004 <108353603+kazmik05102004@users.noreply.github.com>
2022-10-07 02:18:36 +0300

    Delete LR-3 directory

--- LR-3/Colors.java
+++
@@ -1,7 +0,0 @@
-package game.Interface;
-
-public class Colors {
-    public static final String red = "\u001B[31m";
-    public static final String green = "\u001B[32m";
-    public static final String yellow = "\u001B[33m";
-}
--- LR-3/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import static game.Interface.Menu.menu;
-
-public class Main {
-    public static void main(String[] args) {
-        menu();
-    }
-}
